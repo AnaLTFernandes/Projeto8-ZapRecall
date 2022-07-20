48722b8fe73d7c57c101914e50592acbaf1e1a53
Ana Luiza Teixeira Fernandes <analuizateixeirafernandes184@gmail.com>
2022-07-20 17:51:54 -0300

    Renderização de flashcards finalizada

--- src/components/DeckScreen.js
+++ src/components/DeckScreen.js
@@ -1,10 +1,13 @@
+import Deck from './Deck';
+
 export default function DeckScreen() {
     return (
         <div className="deck-screen">
-            <div className="content-deck-screen">
+            <div className="top-deck-screen">
                 <img src="./images/logo.png" alt="ZapRecall"/>
                 <h1>ZapRecall</h1>
             </div>
+            <Deck />
             <div className="deck-progress">0/4 CONCLUÍDOS</div>
         </div>
     );
