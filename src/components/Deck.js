e3ed3791347e78500318e365a9b687cc3f6ebe54
Ana Luiza Teixeira Fernandes <analuizateixeirafernandes184@gmail.com>
2022-07-21 14:51:34 -0300

    Flashcards: mudança de faces completa

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,47 +1,113 @@
+import React from "react";
+
 const deck = [
     {
         question:'O que é JSX?',
-        answer:'Uma extensão de linguagem do JavaScript'
+        answer:'Uma extensão de linguagem do JavaScript',
+        status: '', // progress-ask/progress-answer/concluded
+        result: '' // correct-good/correct/incorrect
     },
     {
         question:'O React é __ ',
-        answer:'uma biblioteca JavaScript para construção de interfaces'
+        answer:'uma biblioteca JavaScript para construção de interfaces',
+        status: '',
+        result: ''
     },
     {
         question:'Componentes devem iniciar com __',
-        answer:'letra maiúscula'
+        answer:'letra maiúscula',
+        status: '',
+        result: ''
     },
     {
         question:'Podemos colocar __ dentro do JSX ',
-        answer:'expressões'
+        answer:'expressões',
+        status: '',
+        result: ''
     },
     {
         question:'O ReactDOM nos ajuda __ ',
-        answer:' interagindo com a DOM para colocar componentes React na mesma'
+        answer:' interagindo com a DOM para colocar componentes React na mesma',
+        status: '',
+        result: ''
     },
     {
         question:'Usamos o npm para __ ',
-        answer:'gerenciar os pacotes necessários e suas dependências'
+        answer:'gerenciar os pacotes necessários e suas dependências',
+        status: '',
+        result: ''
     },
     {
         question:'Usamos props para __ ',
-        answer:'passar diferentes informações para componentes'
+        answer:'passar diferentes informações para componentes',
+        status: '',
+        result: ''
     },
     {
         question:'Usamos estado (state) para __ ',
-        answer:'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
+        answer:'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
+        status: '',
+        result: ''
     },
 ]
 
-export default function Deck() {
+function sort() {
+    deck.sort(() => Math.random() -0.5);
+}
+
+sort();
+
+function Flashcard ({flashcard, index}) {
+    const {question, answer, status, result} = flashcard;
+
+    const [cardStatus, setCardStatus] = React.useState(status);
+
+    const className = (`flashcard ${cardStatus} ${result}`).trim();
+
+    let template;
+
+    if(cardStatus === '') {
+        template = (
+            <div key={index} className={className}>
+                <span>Pergunta {index+1}</span>
+                <ion-icon name="play-outline" onClick={() => setCardStatus('progress-ask')}></ion-icon>
+            </div>
+        )
+    }
+    else if (cardStatus === 'progress-ask') {
+        template = (
+            <div key={index} className={className}>
+                <span>{question}</span>
+                <img alt='turn' src="./images/setinha.png" onClick={() => setCardStatus('progress-answer')} />
+            </div>
+        )
+    }
+    else if (cardStatus === 'progress-answer') {
+        template = (
+            <div key={index} className={className}>
+                <span>{answer}</span>
+                <div className="options">
+                    <div className="option-1" onClick={() => setCardStatus('concluded')}>Não lembrei</div>
+                    <div className="option-2" onClick={() => setCardStatus('concluded')}>Quase não lembrei</div>
+                    <div className="option-3" onClick={() => setCardStatus('concluded')}>Zap!</div>
+                </div>
+            </div>
+        )
+    }
+
+    
+    return (template);
+}
+
+export default function Deck({setTotal, progress, setProgress}) {
+
+    setTotal(deck.length);
+    
     return (
         <div className="deck">
 
             {deck.map((flashcard, index) => (
-                <div className="flashcard">
-                        <p>Pergunta {index+1}</p>
-                        <ion-icon name="play-outline"></ion-icon>
-                </div>
+                <Flashcard flashcard={flashcard} index={index} />
             ))}
         </div>
     );
--- src/components/DeckScreen.js
+++ src/components/DeckScreen.js
@@ -1,14 +1,18 @@
+import React from 'react';
 import Deck from './Deck';
 
 export default function DeckScreen() {
+    const [total, setTotal] = React.useState(0);
+    const [progress, setProgress] = React.useState(0);
+
     return (
         <div className="deck-screen">
-            <div className="top-deck-screen">
+            <header className="top-deck-screen">
                 <img src="./images/logo.png" alt="ZapRecall"/>
                 <h1>ZapRecall</h1>
-            </div>
-            <Deck />
-            <div className="deck-progress">0/4 CONCLUÍDOS</div>
+            </header>
+            <Deck setTotal={setTotal} progress={progress} setProgress={setProgress}/>
+            <div className="deck-progress">{progress}/{total} CONCLUÍDOS</div>
         </div>
     );
 }
